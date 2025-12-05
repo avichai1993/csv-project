756d572f5adcb1e318efcdeffd19b054449cf7e1
aattias <aattias@proofpoint.com>
2025-12-05 05:42:42 +0200

    Remove generated API client code and configuration files

--- frontend/src/generated/api/models/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './error-response-dto';
-export * from './health-response-dto';
-export * from './target-create-dto';
-export * from './target-dto';
-export * from './target-update-dto';
