093b0186c6829b2eecafc391ecc8f78c635b8a8b
aattias <aattias@proofpoint.com>
2025-12-06 18:00:33 +0200

    Revert "Add generated backend code for Render deployment"
    This reverts commit c6302242c1891462c3afe88331b7fbc9d753d3ca.

--- backend/src/generated/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Auto-generated code from OpenAPI specification
-
-WARNING: Do not edit files in this directory manually.
-They will be overwritten when regenerating from OpenAPI.
-
-To regenerate:
-    poetry run clean-generated
-    poetry run generate-api
-"""
--- backend/src/generated/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Auto-generated route registrations from OpenAPI paths.yaml"""
-
-from .target_routes import register_target_routes
-from .health_routes import register_health_routes
-
-__all__ = ["register_target_routes", "register_health_routes"]
